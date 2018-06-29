33258ec25484b93775de1592ce110373a98fbf17
Lauris Binde <lauris@lauris.lv>
2018-06-29 09:46:35 +0200

    Ask for confirmation before replacing configuration from preset to prevent accidental override

--- src/plib/resources/locales/en-US.php
+++ src/plib/resources/locales/en-US.php
@@ -1,6 +1,10 @@
 <?php
 
 $messages = [
+    'index.config.confirm.popup.cancel'     => 'Cancel',
+    'index.config.confirm.popup.content'    => 'Are you sure you want to replace existing configuration with the one from choosen preset? This action cannot be undone!',
+    'index.config.confirm.popup.replace'    => 'Replace',
+    'index.config.confirm.popup.title'      => 'Confirm configuration change',
     'index.config.label.json'               => 'Current JSON configuration:',
     'index.config.label.preset'             => 'Load configuration from a preset:',
     'index.config.message.success'          => 'JSON configuration saved successfully',
